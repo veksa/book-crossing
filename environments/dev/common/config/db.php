842c78b20b43b27cce7c0ce2bc15d97241ab1b79
alex <admin@devblog.pro>
2016-11-19 14:28:03 +0500

    fix database name and add phpmyadmin route

--- environments/dev/common/config/db.php
+++ environments/dev/common/config/db.php
@@ -1,7 +1,7 @@
 <?
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=book-crossing',
+    'dsn' => 'mysql:host=localhost;dbname=book_crossing',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
